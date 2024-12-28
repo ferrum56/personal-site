ae8883ef34f8b3fa87a437813c7a883a6a812934
Alan <alanzyt0512@gmail.com>
2024-12-28 01:47:48 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Personal Website
+
+The repository for my personal website. A simple and modern TypeScript Vue-based website.
+
+### Technologies
+
+Important technologies and their versions are listed here. For all packages check the `package.json` file.
+
+- Node 20.13.1 
+- Yarn 4.5.3
+- Vite 6.0.5
+- TypeScript 5.7.2
+- Vue 3.5.13
+- ESLint 9.17.0
+- Prettier 3.4.2
+- Vitest 2.1.8
---
+++ eslint.config.mjs
@@ -0,0 +1,68 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import pluginVue from 'eslint-plugin-vue';
+
+export default tseslint.config(
+    {
+        files: ['**/*.{js,ts,vue}'],
+    },
+    {
+        ignores: ['node_modules/', "dist/", ".yarn/", '**/*.mjs', '**/*.cjs', '**/*.config.ts', '**/*.d.ts'],
+    },
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    prettierConfig,
+    ...pluginVue.configs["flat/recommended"],
+    {
+        files: ['**/*.vue'],
+        languageOptions: {
+            parserOptions: {
+                parser: tseslint.parser,
+            }
+        },
+        rules: {
+            "vue/first-attribute-linebreak": "off",
+            "vue/html-closing-bracket-newline": ["error", {
+                "singleline": "never",
+                "multiline": "never",
+                "selfClosingTag": {
+                    "singleline": "never",
+                    "multiline": "never",
+                },
+            }],
+            "vue/html-indent": ["error", 4, {
+                "attribute": 1,
+                "baseIndent": 1,
+                "closeBracket": 0,
+                "alignAttributesVertically": false,
+                "ignores": [],
+            }],
+            "vue/html-self-closing": ["error", {
+                "html": {
+                    "void": "always",
+                    "normal": "always",
+                    "component": "always",
+                },
+                "svg": "always",
+                "math": "always",
+            }],
+            "vue/max-attributes-per-line": "off",
+            "vue/multiline-html-element-content-newline": "off",
+            "vue/singleline-html-element-content-newline": "off",
+            "vue/component-tags-order": ["error", {
+                "order": ["script", "template", "style"],
+            }],
+        },
+    },
+    {
+        rules: {
+            "no-unused-private-class-members": "warn",
+            "no-empty": "warn",
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "warn",
+        }
+    }
+);
