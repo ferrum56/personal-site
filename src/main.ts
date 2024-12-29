5b8fd4b4497cbe0c5b0f011614c22aade111a1a2
Alan <alanzyt0512@gmail.com>
2024-12-29 13:52:53 -0800

    chore: set up Quasar framework

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
 import { createRouter } from 'vue-router';
+import { Quasar } from 'quasar';
+import 'quasar/dist/quasar.css';
 
 import App from './App.vue';
 import routerOptions from '@/router';
 import './style.css';
 
 const router = createRouter(routerOptions);
-
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(Quasar, { plugins: {} }).mount('#app');
