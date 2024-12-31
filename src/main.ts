0f47d87ce2245da90530675854acdafac782b800
Alan <alanzyt0512@gmail.com>
2024-12-31 03:44:04 -0800

    style: add the NovecentoSansWide font

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import 'quasar/dist/quasar.css';
 import App from './App.vue';
 import routerOptions from '@/router';
 import './style.css';
+import './font.css';
 
 const router = createRouter(routerOptions);
 createApp(App).use(router).use(Quasar, { plugins: {} }).mount('#app');
