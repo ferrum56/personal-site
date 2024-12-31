14a5fba609e0aa990379414ec1175cf78643d9b6
Alan <alanzyt0512@gmail.com>
2024-12-31 07:52:29 -0800

    build: fix small build import issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+/// <reference types="vitest/config" />
 import { fileURLToPath, URL } from 'node:url';
 
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
@@ -16,6 +16,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
+            '~': fileURLToPath(new URL('./', import.meta.url)),
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
