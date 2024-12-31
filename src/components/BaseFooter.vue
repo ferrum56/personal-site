4fccfa797c80b0a7add3cd35e4180e98d7c3cc75
Alan <alanzyt0512@gmail.com>
2024-12-30 23:28:08 -0800

    feat: add a simple footer to the website

---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import socialLinks from '/content/social-links';
+</script>
+
+<template>
+    <q-footer elevated class="bg-grey-8 text-white">
+        <div class="row justify-center items-center">
+            <q-btn v-for="link in socialLinks" :key="link.name" :icon="link.icon" :href="link.url" target="_blank"
+                flat dense round class="q-ml-sm q-mr-sm text-white" aria-label="link.name" />
+        </div>
+        <div class="text-center q-mt-sm q-mb-md">
+            © 2024 -- {{ new Date().getFullYear() }} Alan Zheng. All Rights Reserved.
+        </div>
+    </q-footer>
+</template>
+
+<style scoped>
+
+</style>
