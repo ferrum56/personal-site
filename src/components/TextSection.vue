485d12331a0c6bdda9d3aae12cc97d88142df2ed
Alan <alanzyt0512@gmail.com>
2025-01-04 19:05:25 -0800

    style: initial design for homepage

--- src/components/TextSection.vue
+++ src/components/TextSection.vue
@@ -7,6 +7,7 @@ import type { TextSectionContent } from '@/content/types';
 
 defineProps<{
     content: TextSectionContent;
+    sectionNumber: string;
 }>();
 
 let linkIndex = 0;
@@ -14,8 +15,9 @@ onBeforeUpdate(() => linkIndex = 0);
 </script>
 
 <template>
-    <div>
-        <h3 class="font-nsw all-caps">{{ content.title }}</h3>
+    <div class="text-section-elem">
+        <h5 class="font-nsw all-caps">{{ content.title }}</h5>
+        <hr />
         <template v-if="isString(content.text)">
             <p class="font-tw">{{ content.text }}</p>
         </template>
@@ -28,15 +30,72 @@ onBeforeUpdate(() => linkIndex = 0);
             </p>
         </template>
         <slot />
+
+        <div class="section-header font-b">Archive ://</div>
+        <span class="section-numer font-b">// No. {{ sectionNumber }}</span>
+        <div class="corner-shape corner-bl" />
+        <div class="corner-shape corner-br" />
     </div>
 </template>
 
 <style scoped>
-h2 {
+.text-section-elem {
+    position: relative;
+    margin: 2rem 0;
+    padding: 1rem 2rem;
+    background-color: var(--theme-white);
+    border: solid 1px var(--theme-sky);
+}
+
+h5 {
     font-weight: 700;
+    margin: 1rem 0;
 }
 
 p {
     font-size: 1rem;
+}
+
+.section-header {
+    position: absolute;
+    top: -0.75rem;
+    left: 1rem;
+    padding: 0 0.5rem;
+    font-size: 0.7rem;
+    background-color: var(--theme-white);
+    border: solid 1px var(--theme-sky);
+}
+
+.section-numer {
+    position: absolute;
+    top: 0;
+    right: 5px;
+}
+
+.corner-shape {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    clip-path: polygon(0 0, 100% 0, 100% 20%, 20% 20%, 20% 100%, 0 100%);
+    background: repeating-linear-gradient(
+        45deg,
+        var(--theme-sky),
+        var(--theme-sky) 2px,
+        var(--theme-white) 2px,
+        var(--theme-white) 4px
+    );
+    opacity: 0.5;
+}
+
+.corner-bl {
+    bottom: 5px;
+    left: 5px;
+    transform: rotate(270deg);
+}
+
+.corner-br {
+    bottom: 5px;
+    right: 5px;
+    transform: rotate(180deg);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,16 +7,26 @@ import intro from '@/content/intro';
 </script>
 
 <template>
-    <q-page>
-        <text-section :content="intro.person">
-            <q-btn outline square label="Learn More" to="/about" />
+    <q-page class="home-view-elem">
+        <text-section :content="intro.person" section-number="001">
+            <q-btn outline square label="Learn More" to="/about" class="text-section-btn" />
         </text-section>
-        <text-section :content="intro.website">
-            <q-btn :icon="toQIcon(faGithub)" label="Check Source on GitHub" href="https://github.com/ferrum56/personal-site" target="_blank" />
+        <text-section :content="intro.website" section-number="002">
+            <q-btn outline square :icon="toQIcon(faGithub)" label="Check on GitHub"
+                   href="https://github.com/ferrum56/personal-site" target="_blank" class="text-section-btn" />
         </text-section>
     </q-page>
 </template>
 
 <style scoped>
+.home-view-elem {
+    margin: 1rem 0;
+    padding: 0 3rem;
+    border: dashed 2px lightgrey;
+}
 
+.text-section-btn {
+    color: var(--theme-sky);
+    border-color: var(--theme-sky);
+}
 </style>
