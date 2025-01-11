ac170be8e029f2a80844e7d9fbdd1691a4171f76
Alan <alanzyt0512@gmail.com>
2025-01-11 14:53:13 -0800

    refactor: clean up homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,15 +4,10 @@ import home from '@/content/home';
 </script>
 
 <template>
-    <q-page class="q-ma-lg q-px-lg home-view-elem">
-        <template v-for="(section, idx) in home" :key="idx">
-            <section-container :section-template="section" :no="idx + 1" />
-        </template>
+    <q-page class="q-my-xl q-ml-lg q-mr-xl q-px-md q-pb-md">
+        <section-container v-for="(section, idx) in home" :key="idx" :section-template="section" :no="idx + 1" />
     </q-page>
 </template>
 
 <style scoped>
-.home-view-elem {
-    border: dashed 2px lightgrey;
-}
 </style>
