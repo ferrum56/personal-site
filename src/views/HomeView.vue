009c7741982a27400421afb931b07c4bd46d0279
Alan <alanzyt0512@gmail.com>
2025-01-11 15:48:42 -0800

    style: adjust margins on homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import home from '@/content/home';
 </script>
 
 <template>
-    <q-page class="q-my-xl q-ml-lg q-mr-xl q-px-md q-pb-md">
+    <q-page class="q-my-xl q-ml-lg q-mr-xl q-px-lg q-pt-lg">
         <section-container v-for="(section, idx) in home" :key="idx" :section-template="section" :no="idx + 1" />
     </q-page>
 </template>
