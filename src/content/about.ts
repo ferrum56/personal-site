793124730989293e53d17e3b0e141fe82d442706
Alan <alanzyt0512@gmail.com>
2025-06-04 01:15:18 -0700

    commit before overhaul

---
+++ src/content/about.ts
@@ -0,0 +1,28 @@
+import type { SectionTemplate } from './types';
+
+export default [
+    {
+        title: 'Self Introduction',
+        items: [
+            {
+                type: 'text',
+                content: {
+                    text: [
+                        "Hello again, I'm Alan. I am a software developer with a passion for creating and building things. I have experience in web development, mobile development, and cloud computing. I am always looking for new challenges and opportunities to learn and grow.",
+                    ],
+                },
+            },
+        ],
+    },
+    {},
+    {},
+    {},
+    {
+        title: 'Education',
+        items: [],
+    },
+    {
+        title: 'Experience',
+        items: [],
+    },
+] as SectionTemplate[];
---
+++ src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { format } from 'quasar';
+const { capitalize } = format;
+
+import SectionContainer from '@/components/SectionContainer.vue';
+import about from '@/content/about';
+
+const aboutTabOptions = ['about me (again)', 'history (maybe)', 'hobbies (why not)', 'fun facts (not really)'];
+const aboutTab = ref('about me (again)');
+</script>
+
+<template>
+    <q-page class="q-my-xl q-ml-lg q-mr-xl q-px-lg q-pt-lg">
+        <q-tabs v-model="aboutTab" align="justify" class="font-nsw">
+            <q-tab v-for="option in aboutTabOptions" :key="option" :name="option" :label="capitalize(option)" />
+        </q-tabs>
+        <q-separator />
+        <q-tab-panels v-model="aboutTab" animated>
+            <q-tab-panel v-for="(section, idx) in about.slice(0, 4)" :key="idx" :name="aboutTabOptions[idx]">
+                <section-container :section-template="section" :no="idx + 1" />
+            </q-tab-panel>
+        </q-tab-panels>
+        <section-container :section-template="about[4]" :no="5" />
+        <section-container :section-template="about[5]" :no="6" />
+    </q-page>
+</template>
+
+<style scoped>
+
+</style>
