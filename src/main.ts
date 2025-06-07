48d588a184364f40559604f3d4e0b7295a4f5943
Alan <alanzyt0512@gmail.com>
2025-06-07 00:10:28 -0700

    refactor: clean up main and introduce tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { createApp } from 'vue';
-import { createRouter } from 'vue-router';
-import { Quasar } from 'quasar';
-import 'quasar/dist/quasar.css';
 
 import App from './App.vue';
-import routerOptions from '@/router';
+import { router } from '@/router';
 import './style.css';
 import './font.css';
 
-const router = createRouter(routerOptions);
-router.afterEach((to) => {
-    if (typeof to.name === 'string') {
-        document.title = `${to.name.toUpperCase()} | ALAN ZHENG`;
-    }
-});
-
-createApp(App).use(router).use(Quasar, { plugins: {} }).mount('#app');
+createApp(App).use(router).mount('#app');
