1724cdfcd951102fa8c41a4fc3d976c4e8f29cbd
Alan <alanzyt0512@gmail.com>
2025-06-07 06:57:49 -0700

    chore: clean up tests and code for old dependencies

--- test/router.test.ts
+++ test/router.test.ts
@@ -1,20 +1,10 @@
 import { describe, expect, it } from 'vitest';
 
-it.todo('Following tests are broken due to changed import from quasar');
+import { routes } from '@/router/routes';
 
-// import fullRoutes, { navRoutes } from '@/router/routes';
-//
-// describe('Routes', () => {
-//     it('should have correct value: fullRoutes', async () => {
-//         expect(fullRoutes).to.exist.and.to.be.an('array');
-//         expect(fullRoutes).to.have.nested.property('[0].path', '/');
-//     });
-//
-//     it.todo('should have correct behaviour: dynamic import');
-//
-//     it('should have correct value: navRoutes', async () => {
-//         expect(navRoutes).to.exist.and.to.be.an('array');
-//         expect(navRoutes).to.have.lengthOf(fullRoutes.length);
-//         expect(navRoutes).to.have.nested.property('[0].to', '/');
-//     });
-// });
+describe('router::routes', () => {
+    it('routes has correct value', async () => {
+        expect(routes).to.exist.and.to.be.an('array');
+        expect(routes).to.have.nested.property('[0].path', '/');
+    });
+});
