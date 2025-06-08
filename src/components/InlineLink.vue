c748affa9a226843d7298de580674d9b3f800205
Alan <alanzyt0512@gmail.com>
2025-06-08 10:49:27 -0700

    refactor: redesign in-text styled link

---
+++ src/components/InlineLink.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { isExternalUrl } from '@/utils/string-util';
+
+defineProps<{
+    to: string;
+    label: string;
+}>();
+</script>
+
+<template>
+    <a v-if="isExternalUrl(to)" :href="to" class="inline-link" target="_blank">{{ label }}</a>
+    <RouterLink v-else :to="to" class="inline-link">{{ label }}</RouterLink>
+</template>
+
+<style scoped>
+@reference "@/style.css";
+
+.inline-link {
+    @apply relative no-underline text-ri-glow1 hover:text-ri-glow2;
+    padding-bottom: 2px;
+}
+
+.inline-link::after {
+    @apply absolute left-0 bottom-0 w-0 bg-ri-glow1;
+    content: "";
+    height: 1px;
+    transition: width 0.3s;
+}
+
+.inline-link:hover::after {
+    @apply w-full;
+}
+</style>
