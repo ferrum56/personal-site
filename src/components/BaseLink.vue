137fc5bd5c245920017eecb062a72e6be9f3b97d
Alan <alanzyt0512@gmail.com>
2025-06-09 23:42:14 -0700

    refactor: separate in-text link style from base

---
+++ src/components/BaseLink.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { isExternalUrl } from '@/utils/string-util';
+
+defineProps<{
+    to: string;
+    label: string;
+}>();
+</script>
+
+<template>
+    <span class="no-underline">
+        <a v-if="isExternalUrl(to)" :href="to" target="_blank">{{ label }}</a>
+        <RouterLink v-else :to="to">{{ label }}</RouterLink>
+    </span>
+</template>
---
+++ src/components/TextLink.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import BaseLink from './BaseLink.vue';
+
+defineProps<{
+    to: string;
+    label: string;
+}>();
+</script>
+
+<template>
+    <BaseLink :to="to" :label="label" class="text-link" />
+</template>
+
+<style scoped>
+@reference "@/style.css";
+
+.text-link {
+    @apply relative pb-px text-ri-glow1 hover:text-ri-glow2;
+}
+
+.text-link::after {
+    @apply absolute left-0 bottom-0 w-0 h-px bg-ri-glow1;
+    content: "";
+    transition: width 0.3s;
+}
+
+.text-link:hover::after {
+    @apply w-full;
+}
+</style>
