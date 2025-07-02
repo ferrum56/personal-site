ae74eaedde67fc1c77196f65c7ca7c7c033276d7
Alan <alanzyt0512@gmail.com>
2025-07-02 05:44:04 -0700

    build: add bundle analyzer/visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 /// <reference types="vitest/config" />
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 import svgLoader from 'vite-svg-loader';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue(), tailwindcss(), svgLoader()],
+    plugins: [
+        vue(),
+        tailwindcss(),
+        svgLoader(),
+        visualizer({
+            open: true,
+            gzipSize: true,
+            brotliSize: true,
+        }) as PluginOption,
+    ],
     define: {
         __PROJECT_VERSION__: JSON.stringify(process.env.npm_package_version),
         __EO2024__: JSON.stringify('The intelligent creatures on Sol-3 celebrate their planet\'s completion of' +
