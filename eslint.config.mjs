b4e4f312dcfc7c9e693788d265a9cb761e9f721e
Alan <alanzyt0512@gmail.com>
2025-07-13 02:34:30 -0700

    chore: fix missing globals import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 import prettierConfig from 'eslint-config-prettier';
 import pluginVue from 'eslint-plugin-vue';
 
